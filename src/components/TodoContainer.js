50fead5ec8db382f46d165354b2b7b6dc4d6d9ee
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-04 14:02:07 +0100

    Update completed using setState

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,7 +24,17 @@ class TodoContainer extends React.Component {
   }
 
   handleChange = (id) => {
-    console.log('clicked', id)
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if(todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    }));
   }
 
   render() {
