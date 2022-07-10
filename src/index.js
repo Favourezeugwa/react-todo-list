c29e7b035613adacab053eb6be00362bf45b5526
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-10 14:26:11 +0100

    Create working dir for functionl components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Component file
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
 
 // stylesheet
-import './App.css';
+import './functionBased/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
